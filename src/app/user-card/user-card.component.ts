74877a39cffbd176382e49037dd86c752554ca0f
Apeksha Kumawat <apeksha.kumawat@accolitedigital.com>
2024-04-03 00:44:30 +0530

    Added unsubscribe hook

--- src/app/user-card/user-card.component.ts
+++ src/app/user-card/user-card.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { User } from '../user.interface';
 import { CommonModule } from '@angular/common';
 import { UserService } from '../user.service';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -16,28 +15,30 @@ export class UserCardComponent implements OnInit{
 
   constructor(private userService: UserService, private route: ActivatedRoute, private router: Router){}
 
-  goback() {
+   public goback() {
     this.router.navigate(['/']);
   }
+
+  public createInitials(name: string):void {
+    const namesArray = name.trim().split(' ');
+    if (namesArray.length === 1)
+     console.log(`${namesArray[0].charAt(0)}`);
+    else
+     console.log( `${namesArray[0].charAt(0)}${namesArray[namesArray.length - 1].charAt(0)}`);
+  }
+
+  user: any = '';
+
+  ngOnInit():void{
+    this.getUserById();
+  }
+
+  getUserById(){
+   this.userService.getUsers().subscribe((users) => {
+    const userId = this.route.snapshot.paramMap.get('id') || "";
+      [this.user] = users.filter((user: { id: string; }) => user.id == userId);
+    });  
+  }    
+
   
-    // name: any;
-    // private createInitials():void {
-    //   let initials = this.name.charAt(0).toUpperCase();
-    //   console.log(initials)
-    // }
-    
-    // users: User[] =[];
-    
-    user: any = '';
-
-    ngOnInit():void{
-      this.getUserById();
-    }
-
-    getUserById(){
-      this.userService.getUsers().subscribe((users) => {
-        const userId = this.route.snapshot.paramMap.get('id') || "";
-        [this.user] = users.filter((user: { id: string; }) => user.id == userId);
-      });  
-    }    
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -2,33 +2,35 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
 import { User } from '../user.interface';
 import { RouterModule, Router } from '@angular/router';
-import { UserCardComponent } from "../user-card/user-card.component";
+import { UserCardComponent } from '../user-card/user-card.component';
+import { Subscription } from 'rxjs';
 
 @Component({
-    selector: 'app-user',
-    standalone: true,
-    templateUrl: './user.component.html',
-    styleUrl: './user.component.css',
-    imports: [RouterModule, UserCardComponent]
+  selector: 'app-user',
+  standalone: true,
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+  imports: [RouterModule, UserCardComponent],
 })
 export class UserComponent implements OnInit {
+  id!: number;
 
-id!: number;
-
-constructor(private userService: UserService, private router: Router) {}
+  constructor(private userService: UserService, private router: Router) {}
 
   viewDetails(id: number) {
     this.router.navigate(['user-card', id]);
   }
-
+  subscription: Subscription = new Subscription;
+  isLoading = true;
   users: User[] = [];
 
   ngOnInit(): void {
-    this.userService.getUsers().subscribe((users) => {
-    this.users = users;
-  });
+    this.subscription = this.userService.getUsers().subscribe((users) => {
+      this.users = users;
+      this.isLoading = false;
+    });
   }
-
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
-
-
