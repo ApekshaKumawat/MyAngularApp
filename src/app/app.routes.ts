06349c93fa6c49316b2a21defed151eb8c950451
Apeksha Kumawat <apeksha.kumawat@accolitedigital.com>
2024-04-03 12:28:09 +0530

    Auto-generated commit

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { UserComponent } from './user/user.component';
-import { UserCardComponent } from './user-card/user-card.component';
-
-export const routes: Routes = [
-   {path: '', component: UserComponent},
-   {path: 'user-card/:id',component: UserCardComponent}
-];
